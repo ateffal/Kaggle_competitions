1a29535fe0df15322d45597c9cf0bca9a36bead1
Amine TEFFAL <a.teffal@gmail.com>
2018-01-08 09:40:14 +0000

    Puttin functions in separate file

--- digit_reco_nnet.py
+++ digit_reco_nnet.py
@@ -11,8 +11,10 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 from sklearn.model_selection import train_test_split
 from sklearn import svm
-
 from sklearn.neural_network import MLPClassifier
+
+import image_func.py as imf
+
 
 #reading and splitting data
 labeled_images = pd.read_csv('train.csv')
@@ -40,15 +42,19 @@ plt.title(train_labels.iloc[i])
 plt.hist(train_images.iloc[i])
 
 #Training the model
-clf = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(5, 2), random_state=1)
 X = train_images.values
 y = train_labels.values.ravel(4000,)
+
+clf = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(250,10, ), random_state=1,activation='logistic')
 clf.fit(X, y)
 
 
 #Predicting on test data
 test_data=pd.read_csv('test.csv')
 test_data[test_data>0]=1
+clf.score(test_images,test_labels)
+
+
 results=clf.predict(test_data[0:5000].values)
 
 #Saving predictions to a file
@@ -63,32 +69,9 @@ img=test_data.iloc[i].as_matrix().reshape((28,28))
 plt.imshow(img,cmap='binary')
 plt.title(test_data.index[i])
 
-#function to transform an image into a line of dataframe having the structure
-#of test_data
-def numeriser(file):
-    pixels=[]
-    im = Image.open(file)
-    pix=im.load()
-    ligne=im.size[0]
-    colonne=im.size[1]
-    for j in range(0,ligne):
-        for i in range(0,colonne):
-            pixels=pixels+[sum(pix[i,j])/3]
-    zz=[tuple(pixels)]
-    df = pd.DataFrame.from_records(zz, columns=list(test_data.columns))
-    return df
-    
-#function to predict an image structured as a dataframe
-def predict_image(df_):
-    df_[df_>0]=1
-    return clf.predict(df_)
-    
-    
-#function to visualize an image stored as dataframe
-def visualize_image(df):
-    img=df.iloc[0].as_matrix().reshape((28,28))
-    plt.imshow(img,cmap='binary')
-    plt.title(train_labels.iloc[i])
+imf.predict_image(imf.numeriser("digit_1_bis.jpg"))
+
+
     
     
     
